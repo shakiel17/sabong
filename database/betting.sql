3c96673aa9bea5e831b49dccfc8076b05a2892b9
shakiel17 <hisoka.aboy@gmail.com>
2025-06-03 15:40:32 +0800

    Create betting.sql

---
+++ database/betting.sql
@@ -0,0 +1,149 @@
+/*
+SQLyog Community v13.3.0 (64 bit)
+MySQL - 10.4.32-MariaDB : Database - betting
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`betting` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `betting`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `cash_in` */
+
+DROP TABLE IF EXISTS `cash_in`;
+
+CREATE TABLE `cash_in` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `refno` varchar(100) DEFAULT NULL,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `status` varchar(100) DEFAULT 'pending',
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  `date_received` date DEFAULT NULL,
+  `time_received` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `cash_out` */
+
+DROP TABLE IF EXISTS `cash_out`;
+
+CREATE TABLE `cash_out` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `refno` varchar(100) DEFAULT NULL,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `status` varchar(100) DEFAULT 'pending',
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `customer_account` */
+
+DROP TABLE IF EXISTS `customer_account`;
+
+CREATE TABLE `customer_account` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `fight` */
+
+DROP TABLE IF EXISTS `fight`;
+
+CREATE TABLE `fight` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `fight_no` int(11) DEFAULT NULL,
+  `odds_meron` double DEFAULT NULL,
+  `odds_wala` double DEFAULT NULL,
+  `status` varchar(100) DEFAULT 'open',
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `fight_details` */
+
+DROP TABLE IF EXISTS `fight_details`;
+
+CREATE TABLE `fight_details` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `fight_no` int(11) DEFAULT NULL,
+  `customer_id` varchar(100) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `bet_side` varchar(100) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `fight_result` */
+
+DROP TABLE IF EXISTS `fight_result`;
+
+CREATE TABLE `fight_result` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `fight_no` int(11) DEFAULT NULL,
+  `meron_amount` double DEFAULT NULL,
+  `wala_amount` double DEFAULT NULL,
+  `win_result` varchar(100) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) DEFAULT NULL,
+  `role` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
